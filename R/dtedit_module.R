31b061b959f4f81b6bd550548f8239731e05f03b
Paul Campbell <pacampbell91@gmail.com>
2024-05-11 11:52:55 +0200

    return edited data as a reactive

--- R/dtedit_module.R
+++ R/dtedit_module.R
@@ -5,8 +5,8 @@
 #' @seealso DTedit::dtedit_server
 #' @export
 dtedit_ui <- function(id) {
-	ns <- NS(id)
-	uiOutput(ns(id))
+  ns <- NS(id)
+  uiOutput(ns(id))
 }
 
 #' Shiny Server Module version of DTedit
@@ -14,17 +14,21 @@ dtedit_ui <- function(id) {
 #' @param id An ID string that corresponds with the ID used to call the module's
 #'        UI function.
 #' @param ... other parameters passed to [DTedit::dtedit].
+#' @return a shiny reactive data.frame containing the edited version of the
+#' 	data originally passed to the module
 #' @seealso DTedit::dtedit_ui
 #' @export
 dtedit_server <- function(id, ...) {
-	shiny::moduleServer(
-		id,
-		function(input, output, session) {
-			return(dtedit(input = input,
-						  output = output,
-						  name = id,
-						  id = id,
-						  ...))
-		}
-	)
+  shiny::moduleServer(
+    id,
+    function(input, output, session) {
+			return(dtedit(
+				input = input,
+				output = output,
+				name = id,
+				id = id,
+				...
+			))
+    }
+  )
 }
